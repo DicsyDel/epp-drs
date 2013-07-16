b327fd08957eca9020f850e41cf589cf02c119e1
Julian Sternberg <admin@ispire.me>
2013-07-17 01:19:54 +0200

    extended preinstall script with cache and log dir create

--- app/preinstall.sh
+++ app/preinstall.sh
@@ -1,2 +1,7 @@
-chmod -R 777 cache
-chmod -R 777 logs
+#!/bin/sh
+[ -d "cache/smarty/" ] || mkdir -p cache/smarty
+[ -d "cache/smarty_bin/" ] || mkdir -p cache/smarty_bin
+[ -d "cache/adodb/" ] || mkdir -p cache/adodb
+[ -d "logs/" ] || mkdir -p logs
+chmod -R 777 cache/
+chmod -R 777 logs/
